706d4df764a1a19e6804ee74472109cbc00d90dd
unknownAuthor <msg.8925@gmail.com>
2022-01-08 02:35:46 -0500

    Simple for loops

---
+++ forLoop.py
@@ -0,0 +1,18 @@
+#! /bin/python3
+
+myList = [1, 2, 3, 4, 5]
+
+for num in myList:
+    num = num + 10;
+    print(num)
+
+print(myList)
+
+
+
+mySecondList = ["John", "Rose", "Mike", "Peter", "Sarah"]
+
+for name in mySecondList:
+    print(name)
+
+print(mySecondList)
